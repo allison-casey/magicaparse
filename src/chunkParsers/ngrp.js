15034d134a7309a5625e62e30a2398c681a16884
Steven Casey <stevencasey21@gmail.com>
2018-12-10 18:24:41 -0500

    properly loop over child ids

--- src/chunkParsers/ngrp.js
+++ src/chunkParsers/ngrp.js
@@ -1,10 +1,16 @@
 import * as R from "ramda";
 import {readInt, readDict} from "../byteReaders";
+import {readArrayProp} from "../utils";
 
 export const parsenGRP = ({chunk, buffer}) =>
   R.pipe(
     readInt("id"),
     readDict("attributes"),
     readInt("numChildren"),
-    readInt("childNode")
+    payload =>
+      R.pipe(
+        R.path(["chunk", "numChildren"]),
+        readArrayProp("childIDs", readInt("id"), R.__, payload),
+        R.mergeDeepRight({chunk})
+      )(payload)
   )({buffer});

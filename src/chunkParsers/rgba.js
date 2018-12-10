815cdaf6777264dd7bf3b823fe6c63c72ca2abbf
Steven Casey <stevencasey21@gmail.com>
2018-12-09 21:07:17 -0500

    added readArrayProp method

--- src/chunkParsers/rgba.js
+++ src/chunkParsers/rgba.js
@@ -1,41 +1,19 @@
 import * as R from "ramda";
 import {readUByte} from "../byteReaders";
-import {applyN} from "../utils";
-
-const readArrayProp = R.curry((key, callback, repeat, {chunk = {}, buffer}) => {
-  return applyN(callback, repeat)({chunk: {[key]: []}, buffer});
-});
+import {readArrayProp} from "../utils";
 
 export const parseRGBA = ({chunk, buffer}) => {
   const out = readArrayProp(
     "colors",
-    payload => {
-      const color = R.pipe(
-        readUByte("r"),
-        readUByte("b"),
-        readUByte("g"),
-        readUByte("a")
-      )({buffer: payload.buffer});
-      return {
-        chunk: {colors: [...payload.chunk.colors, color.chunk]},
-        buffer: color.buffer
-      };
-    },
+    R.pipe(
+      readUByte("r"),
+      readUByte("g"),
+      readUByte("b"),
+      readUByte("a")
+    ),
     256,
-    {buffer}
+    {chunk, buffer}
   );
 
-  // const out = applyN(payload => {
-  //   const color = R.pipe(
-  //     readUByte("r"),
-  //     readUByte("b"),
-  //     readUByte("g"),
-  //     readUByte("a")
-  //   )({buffer: payload.buffer});
-  //   return {
-  //     chunk: {colors: [...payload.chunk.colors, color.chunk]},
-  //     buffer: color.buffer
-  //   };
-  // }, 256)({chunk: {colors: []}, buffer});
   return {chunk: {...chunk, ...out.chunk}, buffer: out.buffer};
 };
--- src/utils.js
+++ src/utils.js
@@ -9,3 +9,17 @@ export const log = args => {
   console.log(args);
   return args;
 };
+
+export const readArrayProp = R.curry(
+  (key, callback, repeat, {chunk = {}, buffer}) =>
+    applyN(payload => {
+      const out = callback({buffer: payload.buffer});
+      return {
+        chunk: {[key]: [...payload.chunk[key], out.chunk]},
+        buffer: out.buffer
+      };
+    }, repeat)({
+      chunk: {[key]: []},
+      buffer
+    })
+);
